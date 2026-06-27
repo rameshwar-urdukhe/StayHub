4835a2e4effd0f31c7849bf3cf69e712421f4835
Rameshwar Urdukhe <rpatil768093@gmail.com>
2026-06-27 11:30:47 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    https://stayhub-jekv.onrender.com/api,
+  baseURL: "https://stayhub-jekv.onrender.com/api",
 });
 
 export default API;
